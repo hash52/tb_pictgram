037ff340327cab95a7e9b0577c9e6881823c7ace
EC2 Default User <ec2-user@ip-172-31-26-172.us-east-2.compute.internal>
2019-03-05 09:53:18 +0000

    5

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true,format: { with: /[A-Za-z0-9._+]+@[A-Za-z]+.[A-Za-z]/ }
+  validates :name, length: { maximum: 15 }
+  #validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i }
+  
+  has_secure_password
+end
